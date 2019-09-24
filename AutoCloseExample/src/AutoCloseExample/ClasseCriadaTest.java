3975cd9c9e4b78e5a3834bb10b9774fb51139672
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-09-24 09:34:26 -0300

    Outras observações

--- AutoCloseExample/src/AutoCloseExample/ClasseCriadaTest.java
+++ AutoCloseExample/src/AutoCloseExample/ClasseCriadaTest.java
@@ -5,8 +5,10 @@ import java.io.File;
 import java.io.IOException;
 
 class ClasseCriadaTest {
+    // FIXME este método não gera IOException
     @org.junit.jupiter.api.Test
     void close() throws IOException {
+        // FIXME remova o "try" sem utilidade aqui (observe que não provoca nenhum efeito)
         try {
             Assertions.assertThrows(RuntimeException.class, () -> {
                 try(
