59d299a3a7d572859f83e86c1d5f4f843a31ef14
Enio <mandosaldmer@gmail.com>
2019-11-27 21:58:44 -0300

    Entrega dia 27 22h

--- Projeto/esbocoCodigo.java
+++ Projeto/esbocoCodigo.java
@@ -1,3 +1,4 @@
+import java.io.StringWriter;
 import java.util.Dictionary;
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
@@ -5,14 +6,11 @@ import javax.tools.ToolProvider;
 /** Classe responsável por implementar métodos que retornam instâncias de {@see Expressao}
  * de acordo com expressões informadas por meio de strings com expressões matemáticas,
  * ou caminhos de arquivos referentes a Bytecodes previamente compilados.
+ * 
+ * O código abaixo serve apenas para exemplificar o design e não oferece garantia
+ * de qualidade, sendo detalhes de construção de software uma etapa posterior do desenvolvimento.
  */
 class ConstrutorExpressao {
-
-    /** Constante de caminho padrão a ser definido no código para a criação dos Bytecodes caso
-     * não seja provido um por parâmetro na chamada de {@link #constroiExpressao(String)}.
-    */
-    private final String STD_FILE_PATH = "./";
-
     /** Método a ser chamado para retornar uma instância de {@see Expressao}, gerando
      * o bytecode cujo caminho será armazenado nessa instância, e gerando o identificador
      * que será posteriormente usado para recuperar essa instância com o método
@@ -20,12 +18,23 @@ class ConstrutorExpressao {
      * pelo método {@link #novaExpressao(String)}.
      * O caminho do bytecode gerado é definido em {@see #STD_FILE_PATH}.
      * @param expressao A expressão a ser compilada.
-     * @return Instância de {@see Expressao} com a expressao compilada no filepath default.
+     * @return Instância de {@see Expressao} com a expressao compilada no
+     * identifier gerado por {@link #geraIdentifier()}
      */
-    public Expressao constroiExpressao(String expressao) { //TODO: Modificar para que o parametro 'expressao' seja utilizado corretamente e o filepath seja gerado em geraFilePath
-        String filepath = geraFilePath();
-        geraBytecode(filepath);
-        return novaExpressao(filepath);
+
+    public Expressao[] cache;
+
+    public Expressao constroiExpressao(String expressao) {
+        //Gera um identifier para a expressão existente
+        String identifier = geraIdentifier(expressao);
+        //Se a expressão existir no cache, retornar a expressão.
+        for (Expressao ex : cache) {
+            if(ex.identifier == geraIdentifier(expressao)){
+                return ex;
+            }
+        }
+        geraBytecode(expressao);
+        return novaExpressao(identifier);
     }
     
     /** Método a ser chamado para retornar uma instância de {@see Expressao}, a partir do
@@ -33,42 +42,84 @@ class ConstrutorExpressao {
      * @param identifier O identifier gerado durante a compilação inicial da expressão.
      * @return Instância de {@see Expressao} reconstruída do bytecode recuperado pelo identifier.
      */
-    private Expressao recuperaExpressao(String identifier) { //TODO: Inserir chamada dessa expressão quando houver necessidade do uso de cache
+    private Expressao recuperaExpressao(String identifier) {
         novaExpressao(identifier);
         return expressaoRecuperada;
     }
 
     /** Método a ser chamado internamente por essa classe, para fornecer instâncias de
-     * {@see Expressao} inicializadas com base no filepath do bytecode gerado para os métodos
-     * dessa classe. Esse método define também o comportamento do identifier gerado para a
-     * recuperação - O identifier pode ser o próprio filepath.
-     * @param filepath O filepath do bytecode já gerado para a expressão.
-     * @return Instância de {@see Expressao} reconstruída a 
+     * {@see Expressao}. Esse método define também o comportamento do identifier gerado para a
+     * recuperação.
+     * @return Instância de {@see Expressao} reconstruída a partir do identifier.
      */
-    private Expressao novaExpressao(String filepath) {
+    private Expressao novaExpressao(String identifier) {
 		Expressao novaExpressao = new Expressao();
-        novaExpressao.identifier = filepath;
-        novaExpressao.classFilePath = filepath;
-        //TODO: Chamar cacheiaExpressao() para guardar na classe gerada o bytecode pronto para reutilizar
+        novaExpressao.identifier = identifier;
 		return novaExpressao;
 	}
 
-	private ConstrutorExpressao geraBytecode(String filepath) { //TODO: Obter código de https://github.com/kyriosdata/parser/issues/1
-    
+	private byte[] geraBytecode(String expressao) {
+        //É obtida a referência do arquivo gerado contendo a expressão
+        File arquivoExpressao = geraArquivoExpressao(expressao);
+        /* ...Restante das operações de IO... */
+
+        //O javaCompiler é instanciado
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        try (
+            //São inicializados os recursos, um para obter a classe de expressão a ser compilada, outro para obter
+            //a saída da CompilationTask do JavaCompiler.
+            StandardJavaFileManager fm = compiler.getStandardFileManager(null, null, null);
+            StringWriter out = new StringWriter();
+        ) {
+            //É criado um input uma array do tipo JavaFileObject para ser recebida pelo getTask 
+            Iterable<? extends JavaFileObject> input =
+                    fm.getJavaFileObjects(/*Arquivo contendo a expressão transformada em classe java*/);
+
+            /*
+                SOBRE O ARQUIVO JAVA CONTENDO A EXPRESSÃO:
+                -----
+                O arquivo deverá ter apenas uma classe com o nome do identifier gerado,
+                e um método main contendo a expressão.
+
+                A concatenação das strings para a geração desse arquivo será implementada
+                no método geraArquivoExpressao() dessa classe.
+            
+            */
+                    
+
+            //É chamada a CompilationTask e convertido o output dela em bytes para serem executados na expressão
+            compiler.getTask(out, fm, null, null, null, input).call();
+            return out.toString().getBytes();
+        }
 	}
 
-    private String geraFilePath(){}
+    /* Gera identifier para cada classe */
+    private String geraIdentifier(String expressao){
+        return expressao.hashCode();
+    }
 
-    private ? cacheiaExpressao(){}
+    /* Gera o boilerplate com a classe com o nome do identifier,
+    seguido da declaração da main, concatenada com a expressão executável. */
+    private File geraArquivoExpressao(String expressao){}
 }
 
+//Essa classe guardará os dados da expressão
 class Expressao {
-
     String identifier;
-    String classFilePath;
-    //TODO: Adicionar atributo de cache para essa expressão
+    byte[] expressaoCompilada;
 
-    public Double execute(Dictionary<String, Integer> parametros) {
-        /* definir do codigo do GeradorTest em https://github.com/kyriosdata/parser/issues/1 */
+    //Essa classe executa o método main da expressão convertida em arquivo
+    public Double execute(Dictionary<String, Integer> parametros, byte[] b, String classname) {
+        DynamicClassLoader loader = new ClassLoader();
+        Class<?> clazz = loader.defineClass(classname, b);
+        Method method = clazz.getMethod("main", String[].class);
+        method.invoke(null, (Object) new String[0]);
+    }
+}
+
+//Essa classe é um wrapper da ClassLoader exigido para o overwrite de ClassLoader.defineClass
+class DynamicClassLoader extends ClassLoader {
+    public Class<?> defineClass(String name, byte[] b) {
+        return defineClass(name, b, 0, b.length);
     }
 }
